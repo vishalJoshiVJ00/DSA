df0bc93eee031e9bcee3eaa030f9ed341e597335
vishalJoshiVJ00 <vishaljoshivj00100@gmail.com>
2026-07-09 20:21:13 +0530

    new

--- src/First.java
+++ src/First.java
@@ -1,3 +1,12 @@
-void  main(){
+/*void  main(){
     System.out.println("hello,world!");
+} */
+
+
+//classical code
+
+public class First {
+    public static void main() {
+        System.out.printf("hello, world!");
+    }
 }
